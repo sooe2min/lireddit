48ff27fd3541c75fca1610cb3cb75ee1ae56ca74
Soomin Jang <jsmsumin2@gmail.com>
2021-05-21 02:10:27 +0900

    Apollo Server & Express setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,29 @@
-import { MikroORM } from "@mikro-orm/core"
-import microConfig from "./mikro-orm.config"
-import { Post } from "./entities/Post"
+import { MikroORM } from '@mikro-orm/core'
+import microConfig from './mikro-orm.config'
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { HelloResolver } from './resolvers/hello'
 
 const main = async () => {
-  const orm = await MikroORM.init(microConfig)
-  await orm.getMigrator().up()
+	const orm = await MikroORM.init(microConfig)
+	await orm.getMigrator().up()
 
-  const post = orm.em.create(Post, { title: "my first post" })
-  orm.em.persistAndFlush(post)
+	const app = express()
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [HelloResolver],
+			validate: false
+		})
+	})
 
-  // 클래스를 만들 때만 엔티티의 new Date()가 호출된다.
-  // 클래스를 새로 create하지 않을 땐 default가 필요하다.
-  // nativeInsert는 클래스를 새로 만들지 않고 내가 원하는 데이터만 추가한다. 나머지는 dafeault를 따른다.
-  // await orm.em.nativeInsert(Post, { title: "my second post" })
-  // const posts = await orm.em.find(Post, {})
-  // console.log(posts)
+	apolloServer.applyMiddleware({ app })
+
+	// app.listen(4000, () => {
+	// 	console.log('server started on http://localhost:4000')
+	// })
 }
 
 main().catch(err => {
-  console.log(err)
+	console.log(err)
 })
