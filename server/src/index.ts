445d1678091d32e3d5bd0e2914a86a919082aceb
Soomin Jang <jsmsumin2@gmail.com>
2021-05-21 23:42:12 +0900

    MikroORM TypeGraphQL CRUD

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
+import 'reflect-metadata'
 import { MikroORM } from '@mikro-orm/core'
 import microConfig from './mikro-orm.config'
 import express from 'express'
 import { ApolloServer } from 'apollo-server-express'
 import { buildSchema } from 'type-graphql'
 import { HelloResolver } from './resolvers/hello'
+import { PostResolver } from './resolvers/post'
 
 const main = async () => {
 	const orm = await MikroORM.init(microConfig)
@@ -12,16 +14,17 @@ const main = async () => {
 	const app = express()
 	const apolloServer = new ApolloServer({
 		schema: await buildSchema({
-			resolvers: [HelloResolver],
+			resolvers: [HelloResolver, PostResolver],
 			validate: false
-		})
+		}),
+		context: () => ({ em: orm.em })
 	})
 
 	apolloServer.applyMiddleware({ app })
 
-	// app.listen(4000, () => {
-	// 	console.log('server started on http://localhost:4000')
-	// })
+	app.listen(4000, () => {
+		console.log('server started on http://localhost:4000')
+	})
 }
 
 main().catch(err => {
